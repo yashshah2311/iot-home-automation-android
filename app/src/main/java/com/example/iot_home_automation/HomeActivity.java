933e9fda19570ff92c47f62302c6de50e70aeab6
saman <samaneghbali1@gmail.com>
2021-04-22 01:43:29 -0400

    Add fridge activity

--- app/src/main/java/com/example/iot_home_automation/HomeActivity.java
+++ app/src/main/java/com/example/iot_home_automation/HomeActivity.java
@@ -111,7 +111,12 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
                     startActivity(intent);
                     break;
                 case R.drawable.fridge:
+                    deviceKey = fridgeKey;
                     Toast.makeText(this, "The fridge was called successfully", Toast.LENGTH_LONG).show();
+                    intent=new Intent(HomeActivity.this,FridgeActivity.class);
+                    intent.putExtra("user", username);
+                    intent.putExtra("deviceKey", deviceKey);
+                    startActivity(intent);
                     break;
                 case R.drawable.soil_moisture:
                     deviceKey = soilKey;
